703bd871bd68a504d30f773da9476eab3a0b6e42
Aatosh <sharma.aatosh1416@gmail.com>
2025-03-06 11:03:21 +0545

    blog card changes done

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -1,7 +1,47 @@
-
+import styles from '../Footer/Footer.module.css'
+import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoSend } from "react-icons/io5";
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer >
+    <div className={styles.upper_div}>
+      <h2>Get connected with us in our social sites:</h2>
+       <div className={styles.icons_div}>
+               <AiFillInstagram className={styles.icon}/>
+               <FaFacebook className={styles.icon}/>
+               <FaSquareXTwitter className={styles.icon}/>
+               </div>
+    </div>
+
+    <div className={styles.lower_div}>
+      <div className={styles.div_1}>
+          <h2>Blog Spot</h2>
+          <p>Unleash Ideas, Explore Stories, and Share Knowledge!</p>
+      </div>
+
+      <div className={styles.div_2}>
+        <h2>Quick Links</h2>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Blogs</li>
+        </ul>
+      </div>
+
+      <div className={styles.div_3}>
+          <h2>Follow us for more updates</h2>
+          <div className={styles.input_con}>
+            <input type="email"  placeholder='Enter your email'/>
+            <IoSend className={styles.send_icon}/>
+          </div>
+      </div>
+
+    </div>
+
+   </footer>
   )
 }
 
