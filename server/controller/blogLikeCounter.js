98fd825a5d30746bd5582d034c901ae8ef8a0e31
AneshKarki <aneshkarki07@gmail.com>
2025-03-31 19:17:49 +0545

    small fix: update blogLikeCounter and getBlogRoute

--- server/controller/blogLikeCounter.js
+++ server/controller/blogLikeCounter.js
@@ -1,8 +1,8 @@
-import { createBlog } from "../model/createBlog.js";
+import { Blog } from "../model/createBlog.js";
 export const blogLikeCounter = async (req, res) => {
   const { blogId, userId } = req.body;
   try {
-    const blogDetails = await createBlog.findById(blogId);
+    const blogDetails = await Blog.findById(blogId);
     if (!blogDetails) {
       return res.status(404).json({ message: "blog does not exist" });
     }
@@ -11,7 +11,7 @@ export const blogLikeCounter = async (req, res) => {
       await blogDetails.save();
       return res
         .status(200)
-        .json({ message: "Blog liked successfully", blog:blogDetails });
+        .json({ message: "Blog liked successfully", blog: blogDetails });
     } else {
       blogDetails.blogLikedUser.pull(userId);
       await blogDetails.save();
--- server/route/getBlogRoutes.js
+++ server/route/getBlogRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getBlogs, getBlogsByCategory,getFullBlog } from "../controller/getBlogController";
+import {
+  getBlogs,
+  getBlogsByCategory,
+  getFullBlog,
+} from "../controller/getBlogs.js";
 
 const router = express.Router();
 router.get("/", getBlogs);
-router.get("/full/:id", getFullBlog);
 router.get("/:category", getBlogsByCategory);
+router.get("/full/:id", getFullBlog);
 export default router;
